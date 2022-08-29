d4cf013f3a2b0862ae34d3bec6c126a03a3a74cb
Rene <renealejandrosanchezmorales@gmail.com>
2022-08-29 17:19:15 -0400

    refactro code to app listen

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const HOST = '0.0.0.0';
+  const PORT = 8080;
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
@@ -14,6 +16,6 @@ async function bootstrap() {
       },
     }),
   );
-  await app.listen(3000);
+  await app.listen(PORT, HOST);
 }
 bootstrap();
